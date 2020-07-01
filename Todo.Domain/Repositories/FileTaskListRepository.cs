4494634b81d8efbd73d7b532b69d8931dc6f78a8
Raymond Kaleho <ray@kaleho.com>
2020-06-30 22:06:25 -0500

    Almost feature complete with the repository

---
+++ Todo.Domain/Repositories/FileTaskListRepository.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using Todo.Dialect;
+
+namespace Todo.Domain.Repositories
+{
+    public class FileTaskListRepository
+        : ITaskListRepository
+    {
+        private readonly Dictionary<int, TaskList> _list;
+        private readonly string _dataFolder;
+        private readonly string _indexFile;
+        private readonly Dictionary<int, string> _index;
+
+        public FileTaskListRepository()
+        {
+            _list = new Dictionary<int, TaskList>();
+
+            var assemblyLocation = Assembly.GetEntryAssembly().Location;
+
+            var currentFolder = Path.GetDirectoryName(assemblyLocation);
+
+            _dataFolder = Path.Combine(currentFolder, "data");
+
+            if (!Directory.Exists(_dataFolder))
+            {
+                Directory.CreateDirectory(_dataFolder);
+            }
+
+            _indexFile = Path.Combine(_dataFolder, "index.json");
+
+            if (File.Exists(_indexFile))
+            {
+                var json = File.ReadAllText(_indexFile);
+
+                _index = JsonConvert.DeserializeObject<Dictionary<int, string>>(json);
+            }
+        }
+
+        public int Count => 
+            _index.Count;
+
+        public void Add(
+            int key, 
+            TaskList taskList)
+        {
+            if (_index.ContainsKey(key) || 
+                _index.Values.Any(
+                    x => x.Equals(
+                        taskList.Title, 
+                        StringComparison.InvariantCultureIgnoreCase)))
+            {
+                throw new Exception($"Task list cannot be created with {taskList.Title}");
+            }
+
+            var file = Path.Combine(_dataFolder, $"{key}.json");
+
+            if (File.Exists(file))
+            {
+                throw new Exception("Task list file already exists.");
+            }
+
+            File.WriteAllText(
+                file, 
+                JsonConvert.SerializeObject(taskList, Formatting.Indented));
+
+            _index.Add(key, taskList.Title);
+
+            File.WriteAllText(
+                _indexFile, 
+                JsonConvert.SerializeObject(_index, Formatting.Indented));
+        }
+
+        public TaskList GetByKey(
+            int key)
+        {
+            var file = Path.Combine(_dataFolder, $"{key}.json");
+
+            if (!File.Exists(file))
+            {
+                throw new Exception("Task list does not exist.");
+            }
+
+            var taskList =
+                JsonConvert.DeserializeObject<TaskList>(
+                    File.ReadAllText(file));
+
+            return taskList;
+        }
+
+        public void Update(
+            int key, 
+            TaskList taskList)
+        {
+            if (!_index.ContainsKey(key))
+            {
+                throw new Exception("Cannot update task list that has not previously been added.");
+            }
+
+            File.WriteAllText(
+                Path.Combine(_dataFolder, $"{key}.json"), 
+                JsonConvert.SerializeObject(taskList, Formatting.Indented));
+
+            if (_index.ContainsKey(key) && !_index[key].Equals(taskList.Title))
+            {
+                _index[key] = taskList.Title;
+ 
+                File.WriteAllText(
+                    _indexFile, 
+                    JsonConvert.SerializeObject(_index, Formatting.Indented));
+            }
+        }
+    }
+}
--- Todo.Domain/Repositories/ITaskListRepository.cs
+++ Todo.Domain/Repositories/ITaskListRepository.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using Newtonsoft.Json;
-using Todo.Dialect;
+﻿using Todo.Dialect;
 
 namespace Todo.Domain.Repositories
 {
@@ -16,46 +12,9 @@ namespace Todo.Domain.Repositories
 
         TaskList GetByKey(
             int key);
-    }
 
-    public class FileTaskListRepository
-        : ITaskListRepository
-    {
-        private readonly Dictionary<int, TaskList> _list;
-        private readonly string _repositoryFile;
-
-        public FileTaskListRepository()
-        {
-            _list = new Dictionary<int, TaskList>();
-
-            var assemblyLocation = Assembly.GetExecutingAssembly().Location;
-
-            var currentFolder = Path.GetDirectoryName(assemblyLocation);
-
-            _repositoryFile = Path.Combine(currentFolder, "data", "repository.json");
-
-            if (File.Exists(_repositoryFile))
-            {
-                var json = File.ReadAllText(_repositoryFile);
-
-                _list = JsonConvert.DeserializeObject<Dictionary<int, TaskList>>(json);
-            }
-            else
-            { 
-                Directory.CreateDirectory(Path.Combine(currentFolder, "data"));
-            }
-        }
-
-        public int Count { get; }
-
-        public void Add(int key, TaskList taskList)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public TaskList GetByKey(int key)
-        {
-            throw new System.NotImplementedException();
-        }
+        void Update(
+            int key,
+            TaskList taskList);
     }
 }
